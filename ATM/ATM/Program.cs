e05884bbf334ea08d1732c226c1092e828af7e15
Obinna Isiwekpeni <obinnaisiwekpeni@Obinnas-MacBook-Pro.local>
2021-06-10 01:07:36 +0200

    created project

---
+++ ATM/ATM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATM
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
